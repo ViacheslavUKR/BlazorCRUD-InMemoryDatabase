a3f1a06346561d2b244a12d850633bba19102408
burgas275 <burgas275@gmail.com>
2020-05-31 00:33:06 +0300

    Add project files.

---
+++ BlazorCRUD1/DataAccess/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BlazorCRUD1.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCRUD1.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Article> ArticleList { get; set; }
+    }
+}
